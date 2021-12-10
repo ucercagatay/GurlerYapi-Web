16ea4991c1edb64a46f99e0caccb912bb4c9820b
ÖMER MİRAÇ KÖKÇAM <o.mirackokcam@gmail.com>
2021-12-10 14:12:25 +0300

    Ana sayfa bitti (OFİSTE COMMİT)

---
+++ resources/views/Front/layout/app.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gürler Yapı</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="{{asset("favicon.ico")}}">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link href="{{asset("front/css/animate.css")}}" rel="stylesheet">
+    <!-- Icomoon Icon Fonts-->
+    <link href="{{asset("front/css/icomoon.css")}}" rel="stylesheet">
+    <!-- Bootstrap  -->
+    <link href="{{asset("front/css/bootstrap.css")}}" rel="stylesheet">
+    <!-- Flexslider  -->
+    <link href="{{asset("front/css/flexslider.css")}}" rel="stylesheet">
+    <!-- Flaticons  -->
+    <link href="{{asset("front/fonts/flaticon/font/flaticon.css")}}" rel="stylesheet">
+    <!-- Owl Carousel -->
+    <link href="{{asset("front/css/owl.carousel.min.css")}}" rel="stylesheet">
+    <link href="{{asset("front/css/owl.theme.default.min.css")}}" rel="stylesheet">
+    <!-- Theme style  -->
+    <link href="{{asset("front/css/style.css")}}" rel="stylesheet">
+
+    <!-- Modernizr JS -->
+    <script src="{{asset("front/js/modernizr-2.6.2.min.js")}}"></script>
+    <!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+    <script src="{{asset("front/js/respond.min.js")}}"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+<div id="colorlib-page">
+    <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+    <aside id="colorlib-aside" role="complementary" class="border js-fullheight">
+        <h1 id="colorlib-logo"><a href="index.html"><img src="{{asset("images/s2.png")}}" alt=""></a></h1>
+        <nav id="colorlib-main-menu" role="navigation">
+            <ul>
+                <li class="colorlib-active"><a href="index.html">ANA SAYFA</a>
+                    {{--                    <ul>--}}
+                    {{--                        <li><a href="">ssss</a></li>--}}
+                    {{--                        <li><a href="">ssss</a></li>--}}
+                    {{--                        <li><a href="">ssss</a></li>--}}
+                    {{--                    </ul>--}}
+                </li>
+                @foreach($categories as $category)
+                    <li><a href="{{Route('front.categoryPage',['language'=>$category->language,'url_slug'=>$category->url_slug])}}">{{$category->category_name}}</a>
+                        <ul>
+                            @if($category->sub_category   !=NULL)
+                                <li><a href="{{Route('front.sub1_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category'=>$category->sub_category])}}">{{$category->sub_category}}</a></li>
+                            @endif
+                            @if($category->sub_category_2   !=NULL)
+                                <li> <a href="{{Route('front.sub2_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category_2'=>$category->sub_category_2])}}">{{$category->sub_category_2}}</a></li>
+                            @endif
+                            @if($category->sub_category_3   !=NULL)
+                                <li> <a href="{{Route('front.sub3_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category_3'=>$category->sub_category_3])}}">{{$category->sub_category_3}}</a></li>
+                            @endif
+                        </ul></li>
+                @endforeach
+
+            </ul>
+        </nav>
+
+        <div class="colorlib-footer">
+            <p>
+            <ul>
+                <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                <li><a href="#"><i class="icon-twitter2"></i></a></li>
+                <li><a href="#"><i class="icon-instagram"></i></a></li>
+                <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+            </ul>
+            <small>&copy;
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script>  AKSOYUCER Tüm Hakları Saklıdır
+                <div>Designed by <a href="">YUSUF ÇAĞLAR AKSOY</a> | Developed by <a href="">HAKTAN ÇAĞATAY ÜÇER</a></div>
+
+            </small></p>
+        </div>
+
+    </aside>
+    @yield('content')
+
+</div>
+
+<!-- jQuery -->
+<script src="{{asset("front/js/jquery.min.js")}}" ></script>
+<!-- jQuery Easing -->
+<script src="{{asset("front/js/jquery.easing.1.3.js")}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset("front/js/bootstrap.min.js")}}" ></script>
+<!-- Waypoints -->
+<script src="{{asset("front/js/jquery.waypoints.min.js")}}" ></script>
+<!-- Flexslider -->
+<script src="{{asset("front/js/jquery.flexslider-min.js")}}" ></script>
+<!-- Sticky Kit -->
+<script src="{{asset("front/js/sticky-kit.min.js")}}"></script>
+<!-- Owl carousel -->
+<script src="{{asset("front/js/owl.carousel.min.js")}}"></script>
+<!-- Counters -->
+<script src="{{asset("front/js/jquery.countTo.js")}}" ></script>
+
+
+<!-- MAIN JS -->
+<script src="{{asset("front/js/main.js")}}" ></script>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,6 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Gürler Yapı</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <link rel="shortcut icon" href="{{asset("favicon.ico")}}">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
-
-    <!-- Animate.css -->
-    <link href="{{asset("front/css/animate.css")}}" rel="stylesheet">
-    <!-- Icomoon Icon Fonts-->
-    <link href="{{asset("front/css/icomoon.css")}}" rel="stylesheet">
-    <!-- Bootstrap  -->
-    <link href="{{asset("front/css/bootstrap.css")}}" rel="stylesheet">
-    <!-- Flexslider  -->
-    <link href="{{asset("front/css/flexslider.css")}}" rel="stylesheet">
-    <!-- Flaticons  -->
-    <link href="{{asset("front/fonts/flaticon/font/flaticon.css")}}" rel="stylesheet">
-    <!-- Owl Carousel -->
-    <link href="{{asset("front/css/owl.carousel.min.css")}}" rel="stylesheet">
-    <link href="{{asset("front/css/owl.theme.default.min.css")}}" rel="stylesheet">
-    <!-- Theme style  -->
-    <link href="{{asset("front/css/style.css")}}" rel="stylesheet">
-
-    <!-- Modernizr JS -->
-    <script src="{{asset("front/js/modernizr-2.6.2.min.js")}}"></script>
-    <!-- FOR IE9 below -->
-    <!--[if lt IE 9]>
-    <script src="{{asset("front/js/respond.min.js")}}"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-</head>
-<body>
-<div id="colorlib-page">
-    <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-    <aside id="colorlib-aside" role="complementary" class="border js-fullheight">
-        <h1 id="colorlib-logo"><a href="index.html"><img src="{{asset("images/s2.png")}}" alt=""></a></h1>
-        <nav id="colorlib-main-menu" role="navigation">
-            <ul>
-                <li class="colorlib-active"><a href="index.html">ANA SAYFA</a>
-{{--                    <ul>--}}
-{{--                        <li><a href="">ssss</a></li>--}}
-{{--                        <li><a href="">ssss</a></li>--}}
-{{--                        <li><a href="">ssss</a></li>--}}
-{{--                    </ul>--}}
-                </li>
-                    @foreach($categories as $category)
-                    <li><a href="{{Route('front.categoryPage',['language'=>$category->language,'url_slug'=>$category->url_slug])}}">{{$category->category_name}}</a>
-                        <ul>
-                    @if($category->sub_category   !=NULL)
-                    <li><a href="{{Route('front.sub1_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category'=>$category->sub_category])}}">{{$category->sub_category}}</a></li>
-                    @endif
-                    @if($category->sub_category_2   !=NULL)
-                            <li> <a href="{{Route('front.sub2_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category_2'=>$category->sub_category_2])}}">{{$category->sub_category_2}}</a></li>
-                    @endif
-                    @if($category->sub_category_3   !=NULL)
-                            <li> <a href="{{Route('front.sub3_page',['language'=>$category->language,'url_slug'=>$category->url_slug,'sub_category_3'=>$category->sub_category_3])}}">{{$category->sub_category_3}}</a></li>
-                    @endif
-                        </ul></li>
-                    @endforeach
-
-            </ul>
-        </nav>
-
-        <div class="colorlib-footer">
-            <p>
-            <ul>
-                <li><a href="#"><i class="icon-facebook2"></i></a></li>
-                <li><a href="#"><i class="icon-twitter2"></i></a></li>
-                <li><a href="#"><i class="icon-instagram"></i></a></li>
-                <li><a href="#"><i class="icon-linkedin2"></i></a></li>
-            </ul>
-            <small>&copy;
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>  AKSOYUCER Tüm Hakları Saklıdır
-                {{--                    <br>--}}
-                {{--                    Designed by <a href="">YUSUF ÇAĞLAR AKSOY</a> <br>--}}
-                {{--                    Developed by <a href="">HAKTAN ÇAĞATAY ÜÇER</a>--}}
-            </small></p>
-        </div>
-
-    </aside>
-
+@extends('Front.layout.app')
+@section('content')
+    @yield('content')
     <div id="colorlib-main">
         <aside id="colorlib-hero" class="js-fullheight">
             <div class="flexslider js-fullheight">
@@ -448,8 +364,9 @@
                     </div>
                 </div>
             </div>
-                <div class="row">
-                    <div class="icons">
+            <div class="colorlib-narrow-content">
+                <div class="row" >
+                    <div class="icons animate-box" data-animate-effect="fadeInLeft">
                         <img src="{{asset("images/s2.png")}}" alt="">
                         <div class="contact-icons">
                             <div class="contact-icon">
@@ -474,7 +391,7 @@
                         </div>
                     </div>
                     <div class="whiteArea">
-                        <div class=" contact-form">
+                        <div class=" contact-form animate-box" data-animate-effect="fadeInLeft">
                             <div>
                                 <div>
                                     <label for="">Ad-Soyad</label>
@@ -503,40 +420,19 @@
                             <textarea name="" id="" cols="90" rows="3" placeholder="Mesajınız"></textarea>
                             <input type="submit">
                         </div>
-                        <div class="contact-image">
+                        <div class="contact-image animate-box" data-animate-effect="fadeInLeft">
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
                             <img src="{{asset("front/images/contact.jpg")}}" alt="">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam debitis, eius, eligendi fuga impedit inventore officiis, repellat repellendus sequi totam ullam velit. </p>
+                            <label for="">E-Mail</label>
+                            <div><input type="email"> <a href="">Abone Ol</a></div>
+
                         </div>
                     </div>
 
                 </div>
-            <iframe style="    margin-top: 6vh; filter: invert(90%); border: 1px solid #ffffff;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8156.998315639303!2d39.203692281906946!3d38.67597168995606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4076c01523587a39%3A0xdf454f9e53d46568!2zRWxhesSxxJ8gQXRhdMO8cmsgU3RhZHl1bXU!5e0!3m2!1str!2str!4v1638652709377!5m2!1str!2str" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <iframe style="    margin-bottom: -1vh; margin-top: 6vh; filter: invert(90%); border: 1px solid #ffffff;" class="animate-box" data-animate-effect="fadeInLeft" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8156.998315639303!2d39.203692281906946!3d38.67597168995606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4076c01523587a39%3A0xdf454f9e53d46568!2zRWxhesSxxJ8gQXRhdMO8cmsgU3RhZHl1bXU!5e0!3m2!1str!2str!4v1638652709377!5m2!1str!2str" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
-</div>
-
-<!-- jQuery -->
-<script src="{{asset("front/js/jquery.min.js")}}" ></script>
-<!-- jQuery Easing -->
-<script src="{{asset("front/js/jquery.easing.1.3.js")}}"></script>
-<!-- Bootstrap -->
-<script src="{{asset("front/js/bootstrap.min.js")}}" ></script>
-<!-- Waypoints -->
-<script src="{{asset("front/js/jquery.waypoints.min.js")}}" ></script>
-<!-- Flexslider -->
-<script src="{{asset("front/js/jquery.flexslider-min.js")}}" ></script>
-<!-- Sticky Kit -->
-<script src="{{asset("front/js/sticky-kit.min.js")}}"></script>
-<!-- Owl carousel -->
-<script src="{{asset("front/js/owl.carousel.min.js")}}"></script>
-<!-- Counters -->
-<script src="{{asset("front/js/jquery.countTo.js")}}" ></script>
-
-
-<!-- MAIN JS -->
-<script src="{{asset("front/js/main.js")}}" ></script>
-
-</body>
-</html>
+@endsection
 
